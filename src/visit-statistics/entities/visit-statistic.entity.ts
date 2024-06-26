732c13cf9f96005567b9e66b015b571d12ac1b9f
Aldair Gonzalez <aldairgome97@gmail.com>
2024-06-25 20:35:05 -0600

    Creando la entidad de estadísticas de visitas

--- src/visit-statistics/entities/visit-statistic.entity.ts
+++ src/visit-statistics/entities/visit-statistic.entity.ts
@@ -5,18 +5,41 @@ export class VisitStatistic {
   @PrimaryGeneratedColumn()
   visit_statistics_id: number;
 
-  @Column()
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    update: false,
+  })
   date_time: Date;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    update: false,
+  })
   ip_address: string;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    update: false,
+  })
   visited_page: string;
 
-  @Column()
+  @Column({
+    type: 'int',
+    nullable: false,
+    update: false,
+  })
   time_on_page: number;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    update: false,
+  })
   visit_reference: string;
 }
