5fc07667b8c897ffc1d7aecd3886cc783e4a16b7
Aldair Gonzalez <aldairgome97@gmail.com>
2024-06-25 20:31:19 -0600

    Creando la entidad de carrito de compras

--- src/shopping-carts/entities/shopping-cart.entity.ts
+++ src/shopping-carts/entities/shopping-cart.entity.ts
@@ -13,13 +13,27 @@ export class ShoppingCart {
   @PrimaryGeneratedColumn()
   cart_id: number;
 
-  @Column()
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    nullable: true,
+    update: false,
+  })
   created_at: Date;
 
-  @Column()
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    nullable: true,
+  })
   updated_at: Date;
 
-  @OneToOne(() => User)
+  @OneToOne(() => User, {
+    nullable: false,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
   @JoinColumn({ name: 'user_id' })
   user_id: number;
 }
