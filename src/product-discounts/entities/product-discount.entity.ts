c463058a2bf22cdc90358d5e06b3644a4d9850cc
Aldair Gonzalez <aldairgome97@gmail.com>
2024-06-25 20:29:27 -0600

    Creando la entidad de descuento de producto

--- src/product-discounts/entities/product-discount.entity.ts
+++ src/product-discounts/entities/product-discount.entity.ts
@@ -14,17 +14,33 @@ export class ProductDiscount {
   @PrimaryGeneratedColumn()
   product_discount_id: number;
 
-  @Column()
+  @Column({
+    type: 'timestamp',
+    nullable: false,
+    update: false,
+  })
   start_date: Date;
 
-  @Column()
+  @Column({
+    type: 'timestamp',
+    nullable: false,
+    update: false,
+  })
   end_date: Date;
 
-  @OneToOne(() => Product)
+  @OneToOne(() => Product, {
+    nullable: false,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
   @JoinColumn({ name: 'product_id' })
   product_id: number;
 
-  @OneToOne(() => Discount)
+  @OneToOne(() => Discount, {
+    nullable: false,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
   @JoinColumn({ name: 'discount_id' })
   discount_id: number;
 }
